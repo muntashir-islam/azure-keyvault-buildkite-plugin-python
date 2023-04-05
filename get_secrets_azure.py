0f44628138ee3ff77393afa48adf4094f51ed8c1
Muntashir Islam <muntashir@medstack.co>
2023-04-05 13:28:13 -0600

    update

--- get_secrets_azure.py
+++ get_secrets_azure.py
@@ -2,7 +2,7 @@ import os
 import sys
 import requests
 
-def get_secrets_from_azure(secret_keys):
+def get_secrets_from_azure(secret_keys, tags):
     # Authenticate with Azure
     tenant_id = os.environ.get('AZURE_TENANT_IDS')
     client_id = os.environ.get('AZURE_CLIENT_IDS')
@@ -16,6 +16,7 @@ def get_secrets_from_azure(secret_keys):
         'resource': resource
     }
     response = requests.post(url, data=data).json()
+
     # Retrieve the list of secrets from Azure Key Vault
     vault_name = os.environ.get('AZURE_KEY_VAULT_NAME')
     url = f"https://{vault_name}.vault.azure.net/secrets?api-version=2016-10-01"
@@ -23,28 +24,47 @@ def get_secrets_from_azure(secret_keys):
         'Authorization': f"Bearer {response['access_token']}"
     }
     response = requests.get(url, headers=headers).json()
+    secret_values = {}
+
     # Filter the list of secrets to include only those with keys in secret_keys
     secrets = response['value']
-    filtered_secrets = [secret for secret in secrets if secret['id'].split('/')[-1] in secret_keys]
+    if secret_keys:
+        filtered_secrets = [secret for secret in secrets if secret['id'].split('/')[-1] in secret_keys]
     # Retrieve the values of the filtered secrets
-    secret_values = {}
-    for secret in filtered_secrets:
-        secret_name = secret['id'].split('/')[-1]
-        url = f"https://{vault_name}.vault.azure.net/secrets/{secret_name}?api-version=2016-10-01"
-        response = requests.get(url, headers=headers).json()
-        secret_values[secret_name] = response['value']
+        for secret in filtered_secrets:
+            secret_name = secret['id'].split('/')[-1]
+            url = f"https://{vault_name}.vault.azure.net/secrets/{secret_name}?api-version=2016-10-01"
+            response = requests.get(url, headers=headers).json()
+            secret_values[secret_name] = response['value']
+
+      # Filter the list of secrets to include only those with keys in tags
+    if tags:
+        multiple_tags=[]
+        for tag in tags:
+            tag_key = tag.split('=')[0]
+            tag_value = tag.split('=')[1]
+            filter = [x for x in secrets if x['tags'].get(tag_key) == tag_value]
+            for item in filter:
+                secret_name_tag = item['id'].split('/')[-1]
+                #If the secrets has multiple tags and it is already picked
+                if secret_name_tag in multiple_tags:
+                    continue
+                multiple_tags.append(secret_name_tag)
+                #Get secrets values
+                url = f"https://{vault_name}.vault.azure.net/secrets/{secret_name_tag}?api-version=2016-10-01"
+                response = requests.get(url, headers=headers).json()
+                secret_values[secret_name_tag] = response['value']
     return secret_values
 
 if __name__ == '__main__':
     # Retrieve the list of secret keys from command line arguments
     secret_keys = sys.argv[1].split(',')
-
+    # Retrieve the list of secret tags from command line arguments
+    tags = sys.argv[2].split(',')
     # Retrieve the secret values from Azure Key Vault
-    secret_values = get_secrets_from_azure(secret_keys)
+    secret_values = get_secrets_from_azure(secret_keys, tags)
 
-   
-
-    # Output the secret values as environment variable exports
+    # Output the secret values into a artifact file
     with open('shared_vars.sh', 'w') as f:
         for key, value in secret_values.items():
             f.write(f'export {key}={value}\n')
